c33341e2671c79fe415a07a41723b0feddbe2f44
dir-FY24kotakitakamasa <takamasa.kotaki.v702@dirbato.co.jp>
2024-04-24 16:05:50 +0900

    セキュリティ装備

--- TutorialDevelop/src/main/java/com/techacademy/entity/User.java
+++ TutorialDevelop/src/main/java/com/techacademy/entity/User.java
@@ -9,7 +9,10 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PreRemove;
 import jakarta.persistence.Table;
+import jakarta.transaction.Transactional;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -48,5 +51,16 @@ public class User {
     @Length(max=50)
     private String email;
     
+    @OneToOne(mappedBy = "user")
+    private Authentication authentication;
+    
+    @PreRemove
+    @Transactional
+    private void preRemove() {
+        if (authentication!=null) {
+            authentication.setUser(null);
+            
+        }
+    }
 
 }
--- TutorialDevelop/src/main/resources/data.sql
+++ TutorialDevelop/src/main/resources/data.sql
@@ -1,3 +1,4 @@
 insert into user(name, gender, age, email)values("きらめき太郎","男性",27,'taro.kirameki@mail.com');
 insert into user(name, gender, age, email)values("めきめき次郎","男性",25,'taro.kirameki@mail.com');
 insert into user(name, gender, age, email)values("ときめき花子","女性",21,'taro.kirameki@mail.com');
+insert into authentication(login_user, password, valid_date, user_id)values("ktaro", "$2a$08$clh9XaYYznpX9WDqySgiCuUu4znpSeu2oJi5l2Q00UJs42Llrbd7S", "9999-12-31", 1);

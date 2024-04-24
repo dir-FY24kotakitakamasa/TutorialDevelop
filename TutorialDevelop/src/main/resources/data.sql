5d039b0ab82487537393a7af2bd33465c98e583d
dir-FY24kotakitakamasa <takamasa.kotaki.v702@dirbato.co.jp>
2024-04-24 12:20:29 +0900

    ユーザーリストの作成

---
+++ TutorialDevelop/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user(name, gender, age, email)values("きらめき太郎","男性",27,'taro.kirameki@mail.com');
+insert into user(name, gender, age, email)values("めきめき次郎","男性",25,'taro.kirameki@mail.com');
+insert into user(name, gender, age, email)values("ときめき花子","女性",21,'taro.kirameki@mail.com');
